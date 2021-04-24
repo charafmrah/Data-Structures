6a428905be8242b40fb71a696557061e81fe07e7
Charaf Mrah <charaf4charaf@gmail.com>
2021-04-24 05:44:54 +0300

    Added remove and reheap

--- Data_Structures/src/Heap/MaxHeap.java
+++ Data_Structures/src/Heap/MaxHeap.java
@@ -28,4 +28,43 @@ public class MaxHeap < T extends Comparable <? super T>> {
 		heap[newIndex] = newEntry;
 	}
 	
+	public boolean isEmpty() {
+		return lastIndex < 1;
+	}
+	
+	public T removeMax() {
+		T root = null;
+		
+		if(!isEmpty()) {
+			root = heap[1]; //return value 
+			heap[1] = heap[lastIndex]; //create a semiheap
+			lastIndex--; //decrease the size
+			reheap(1); //transform to a heap
+		}
+		return root;
+	}
+
+	private void reheap(int rootIndex) {
+		boolean done = false;
+		T orphan = heap[rootIndex];
+		int leftChildIndex = 2 * rootIndex;
+		
+		while(!done && (leftChildIndex <= lastIndex)) {
+			int largerChildIndex = leftChildIndex; //assume larger
+			int rightChildIndex = leftChildIndex + 1;
+			if((rightChildIndex <= lastIndex) && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0) {
+				largerChildIndex = rightChildIndex;
+			}
+			if(orphan.compareTo(heap[largerChildIndex]) < 0) {
+				heap[rootIndex] = heap[largerChildIndex];
+				rootIndex = largerChildIndex;
+				leftChildIndex = 2 * rootIndex;
+			}
+			else
+				done = true;
+		}
+		heap[rootIndex] = orphan;
+		
+	}
+	
 }
